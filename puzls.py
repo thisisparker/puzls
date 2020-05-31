1859a7670b12f095714fabc3f4c2f8dc583d7132
Parker Higgins <parker@parkerhiggins.net>
2020-05-31 01:38:33 -0400

    add info function and puzzle formatter

--- puzls.py
+++ puzls.py
@@ -4,51 +4,120 @@ import argparse
 import glob
 import os
 import puz
+import sys
+
+from itertools import zip_longest
 
 class PuzEntry():
     def __init__(self, path):
         self.path = path
 
-        self.puzfile = None
-        self.title = None
-        self.author = None
-        self.progress = 0
+        self.puzfile = puz.read(path)
+        self.title = self.puzfile.title
+        self.author = self.puzfile.author
 
-    def output(self, width):
-        width = width - 6 - 2
+        self.fill = self.puzfile.fill
+
+        filled = [char for char in self.fill if char.isalpha()]
+        total = [char for char in self.fill if char != '.']
+
+        self.progress = len(filled)/len(total)
+
+        self.line_width = os.get_terminal_size().columns
+
+    @property
+    def list_output(self):
+        width = self.line_width - 6 - 2
         column = int(width/3)
         inner = column - 4
-        return ('{puzzle.progress:5.0%} {puzzle.title:{column}.{inner}}'
-                '{puzzle.author:{column}.{inner}}{puzzle.path:{column}.{inner}}').format(
-                  column=column, inner=inner, puzzle=self)
+        return (f'{self.progress:5.0%} {self.title:{column}.{inner}}'
+                f'{self.author:{column}.{inner}}{self.path:{column}.{inner}}')
 
-def main():
+    @property
+    def info_output(self):
+        puzzle_rows = ['PUZZLE PROGRESS','']
+        puzzle_rows.extend(format_puzzle(self.fill, self.puzfile.width))
+        puzzle_info = ['PUZZLE INFO', '', self.title, self.author, '',
+                        f'{self.progress:.0%} complete']
+
+        column_width = min(int(self.line_width/2), len(puzzle_rows[-1]) + 10)
+
+        display = zip_longest(puzzle_rows, puzzle_info, fillvalue='')
+
+        display_block = '\n'.join(
+                [f'{row[0]:{column_width}}{row[1]:{column_width}}'
+                    for row in display])
+
+        return display_block
+
+
+
+def format_puzzle(fill, width):
+    fill_rows = [fill[r:r+width] for r in range(0, len(fill), width)]
+
+    formatted_rows = []
+
+    for row in fill_rows:
+        formatted_row = '▐' if row[0] == '.' else '|' 
+        for c in range(len(row) - 1):
+            formatted_row += row[c]
+            if not row[c] == '.' and not row[c+1] == '.':
+                div_char = '|'
+            elif not row[c] == '.' and row[c+1] == '.':
+                div_char = '▐'
+            elif row[c] == '.' and not row[c+1] == '.':
+                div_char = '▌'
+            else:
+                div_char = '.'
+            formatted_row += div_char
+        formatted_row += row[-1]
+        formatted_row += '▌' if row[-1] == '.' else '|'
+
+        formatted_row = formatted_row.replace('-','_').replace('.','█')
+
+        formatted_rows.append(formatted_row)
+
+    return formatted_rows
+
+
+def list_puzzles():
     puz_paths = glob.glob('*.puz')
     puzzles = []
 
     for path in puz_paths:
-        xw = PuzEntry(path)
         try:
-            xw.puzfile = puz.read(path)
+            xw = PuzEntry(path)
         except:
             continue
-        xw.title = xw.puzfile.title
-        xw.author = xw.puzfile.author
         
-        filled = [char for char in xw.puzfile.fill if char.isalpha()]
-        total = [char for char in xw.puzfile.fill if char != '.']
-
-        xw.progress = len(filled)/len(total)
-
         puzzles.append(xw)
 
     sorted_puzzles = sorted(puzzles, key=lambda x: x.progress, reverse=True)
 
-    line_width = os.get_terminal_size().columns
-
     for puzzle in sorted_puzzles:
-        print(puzzle.output(line_width))
+        print(puzzle.list_output)
 
+def puzzle_info(puzfile):
+    try:
+        xw = PuzEntry(puzfile)
+    except:
+        sys.exit(f'Cannot read {puzfile} as a puz file.')
+
+    print(xw.info_output)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--info', '-i')
+
+    args = parser.parse_args()
+
+    if args.info:
+        puzzle_info(args.info)
+
+    else:
+        list_puzzles()
 
 if __name__ == '__main__':
     main()
